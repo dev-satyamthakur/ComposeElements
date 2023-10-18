25bbdce73fad787674d6a31adb9276685d8fcb92
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-18 21:14:09 +0530

    Adding gradient on top of image

--- app/src/main/java/com/satyamthakur/learning/composeelements/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/composeelements/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -61,6 +63,19 @@ fun ImageCard(
                 painter = painter,
                 contentDescription = contentDescription,
                 contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
             )
             Box(
                 modifier = Modifier
